c2ad0c86a33d8529a498cbe2aa8738c7111f272d
nhcarrigan <nhcarrigan@gmail.com>
2021-01-30 15:37:39 -0800

    docs: clarify clean
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>

--- git-commands/git-clean-files.sh
+++ git-commands/git-clean-files.sh
@@ -4,3 +4,9 @@ git clean -idfx
 # d (directories) - recursively travels directories
 # f (force) - bypasses `clean.requireForce` check
 # x - removes only .gitignored files, not manually created untracked files
+
+# Using interactive mode:
+# Select option 2 to filter by pattern
+# Enter `.env` as ignore pattern (this preserves your .env file)
+# Hit enter to apply filter
+# Select 1 to clean everything else
